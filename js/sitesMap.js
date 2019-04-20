7552eaf5498ee313722856dd12e2e075d1920c9b
Konstantin <kpozdnikin@gmail.com>
2019-04-20 12:14:13 +0300

    Master

---
+++ js/sitesMap.js
@@ -0,0 +1,165 @@
+const sites = [
+  {
+    id: 1,
+    name: 'Khauki',
+    area: '',
+    center: [],
+    points: [
+      [62.3615, 32.2400],
+      [62.3420, 32.2640],
+      [62.3330, 32.2420],
+      [62.3210, 32.2520],
+      [62.3120, 32.2400],
+      [62.3210, 32.2240],
+      [62.3600, 32.2240],
+      [62.3615, 32.2400]
+    ],
+    deposits: [
+      {
+        id: '1',
+        name: 'Khauki-1',
+        depositName: 'Block granite (gabbro-diabase)',
+        area: '1.2 km2',
+        center: [62.3555, 32.2345],
+        points: [
+          [62.3620, 32.2351],
+          [62.3540, 32.2440],
+          [62.3540, 32.2253],
+          [62.3600, 32.2253]
+        ]
+      },
+      {
+        id: '2',
+        name: 'Khauki-2',
+        depositName: 'Block granite (gabbro-diabase)',
+        area: '0.38 km2',
+        center: [62.3450, 32.2540],
+        points: [
+          [62.3505, 32.2532],
+          [62.3440, 32.2604],
+          [62.3434, 32.2554],
+          [62.3452, 32.2500]
+        ]
+      },
+      {
+        id: '3',
+        name: 'Khauki-3',
+        depositName: 'Block granite (gabbro-diabase)',
+        area: '0.63 km2',
+        center: [62.3420, 32.2605],
+        points: [
+          [62.3435, 32.2611],
+          [62.3416, 32.2638],
+          [62.3353, 32.2520],
+          [62.3421, 32.2539]
+        ]
+      },
+      {
+        id: '4',
+        name: 'Khauki-4',
+        depositName: 'Block granite (gabbro-diabase)',
+        area: '0.62 km2',
+        center: [62.3400, 32.2330],
+        points: [
+          [62.3415, 32.2253],
+          [62.3420, 32.2220],
+          [62.3345, 32.2410],
+          [62.3340, 32.2330]
+        ]
+      },
+      {
+        id: '5',
+        name: 'Khauki-5',
+        depositName: 'Block granite (gabbro-diabase)',
+        area: '',
+        center: [],
+        points: [
+          [62.3356, 32.2231],
+          [62.3404, 32.2257],
+          [62.3339, 32.2322],
+          [62.3333, 32.2243]
+        ]
+      },
+      {
+        id: '6',
+        name: 'Khauki-6',
+        depositName: 'Block granite (gabbro-diabase)',
+        area: '',
+        center: [],
+        points: [
+          [62.3328, 32.2245],
+          [62.3339, 32.2336],
+          [62.3313, 32.2336],
+          [62.3313, 32.2245]
+        ]
+      },
+      {
+        id: '7',
+        name: 'Khauki-7',
+        depositName: 'Block granite (gabbro-diabase)',
+        area: '',
+        center: [],
+        points: [
+          [62.3250, 32.2240],
+          [62.3250, 32.2320],
+          [62.3210, 32.2320],
+          [62.3230, 32.2240]
+        ]
+      },
+      {
+        id: '8',
+        name: 'Khauki-8',
+        depositName: 'Block granite (gabbro-diabase)',
+        area: '0.76 km2',
+        center: [62.3238, 32.2350],
+        points: [
+          [62.3300, 32.2324],
+          [62.3300, 32.2400],
+          [62.3230, 32.2410],
+          [62.3210, 32.2410],
+          [62.3210, 32.2340]
+        ]
+      },
+      {
+        id: '9',
+        name: 'Khauki-9',
+        depositName: 'Block granite (gabbro-diabase)',
+        area: '',
+        center: [],
+        points: [
+          [62.3310, 32.2409],
+          [62.3310, 32.2442],
+          [62.3330, 32.2452],
+          [62.3220, 32.2420]
+        ]
+      },
+      {
+        id: '10',
+        name: 'Khauki-10',
+        depositName: 'Block granite (gabbro-diabase)',
+        area: '',
+        center: [],
+        points: [
+          [62.3155, 32.2350],
+          [62.3150, 32.2440],
+          [62.3120, 32.2400],
+          [62.3135, 32.2340]
+        ]
+      }
+    ]
+  },
+  {
+    id: 2,
+    name: 'Helga',
+    depositName: 'Block granite (gabbro-diabase)',
+    area: '0.62 km2',
+    center: [],
+    points: [
+      [62.3530, 32.2507],
+      [62.3530, 32.2600],
+      [62.3500, 32.2620],
+      [62.3450, 32.2600]
+    ],
+    deposits: []
+  }
+];
